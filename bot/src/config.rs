f607629d304abd967262bd2d2900f2a30498c3bc
Brod8362 <brod8362@gmail.com>
2024-02-15 20:16:56 -0600

    Add default paths for true/maybe/false

--- bot/src/config.rs
+++ bot/src/config.rs
@@ -1,12 +1,34 @@
 use std::{error::Error, path::Path, fs::read_to_string};
 
-use serde::{Serialize, Deserialize};
+use serde::Deserialize;
 
-#[derive(Debug, Serialize, Deserialize)]
+const TRUE_DEFAULT: &'static str = "true";
+const MAYBE_DEFAULT: &'static str = "maybe";
+const FALSE_DEFAULT: &'static str = "false";
+
+fn true_default_path() -> String {
+    String::from(TRUE_DEFAULT)
+}
+
+fn maybe_default_path() -> String {
+    String::from(MAYBE_DEFAULT)
+}
+
+fn false_default_path() -> String {
+    String::from(FALSE_DEFAULT)
+}
+
+#[derive(Debug, Deserialize)]
 pub struct BotConfig {
     pub token: String,
     pub influx_host: Option<String>,
-    pub influx_database: Option<String>
+    pub influx_database: Option<String>,
+    #[serde(default = "true_default_path")]
+    pub true_folder_path: String,
+    #[serde(default = "maybe_default_path")]
+    pub maybe_folder_path: String,
+    #[serde(default = "false_default_path")]
+    pub false_folder_path: String
 }
 
 pub fn parse_file(path: &Path) -> Result<BotConfig, Box<dyn Error>> {
