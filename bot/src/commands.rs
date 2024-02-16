b24f9e859a6cfb28d59f1f4fee8d7578aa3e7885
Brod8362 <brod8362@gmail.com>
2024-02-15 20:10:37 -0600

    Initial bot framework

---
+++ bot/src/commands.rs
@@ -0,0 +1,25 @@
+use crate::BotData;
+
+type Context<'a> = poise::Context<'a, BotData, Error>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+
+#[poise::command(slash_command)]
+pub async fn roll(
+    ctx: Context<'_>, 
+    #[description = "What will you ask the Adachi cube?"] question: Option<String>
+) -> Result<(), Error> {
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn invite(
+    ctx: Context<'_>
+) -> Result<(), Error> {
+    let bot_id = ctx.framework().bot_id;
+    let invite_url = format!("https://discord.com/api/oauth2/authorize?client_id={}&permissions=2048&scope=bot", bot_id);
+    ctx.say(format!(
+        "Invite Adachi Cube to your server: {}\nJoin the support server: {}", invite_url, "https://discord.gg/K2MDHBZr9j" //TODO: move this invite link somewhere else
+    )).await?;
+    Ok(())
+}
--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,13 +1,23 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, error::Error};
 
-mod config;
+use analytics::AnalyticsClient;
+use serenity::prelude::GatewayIntents;
+
 mod analytics;
+mod commands;
+mod config;
 
 static DEFAULT_CONFIG_PATH: &'static str = "./config.toml";
 
 pub type DiscordClient = serenity::Client;
 
-fn main() {
+pub struct BotData {
+    pub analytics: AnalyticsClient
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    //parse config
     let config_path_str = match env::var("ADACHI_CONFIG_PATH") {
         Ok(t) => t,
         _ => String::from(DEFAULT_CONFIG_PATH)
@@ -21,5 +31,28 @@ fn main() {
         }
     };
 
-    println!("token: {}", bot_config.token);
+    let analytics = AnalyticsClient::new::<String>(None, None)?; //TODO not None here
+
+    let intents = GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![commands::roll()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok( BotData {
+                    analytics: analytics
+                })
+            })
+        })
+        .build();
+    
+    let client = serenity::Client::builder(bot_config.token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
+    Ok(())
 }
